1f1d0e6ca4ca5a8e5a64adb474b9a92e34441023
josehtat <150950561+josehtat@users.noreply.github.com>
2024-09-20 16:51:35 +0200

    ejercicio 3 hecho, y ejercicio 4 en proceso

---
+++ m7/M7fita1/batalla_naval.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+    <table style="border: 1px solid black; border-collapse: collapse;">
+        <?php
+            
+
+        ?>
+    </table>
+</body>
+</html>
---
+++ m7/M7fita1/ex4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ejercicio 4</title>
+</head>
+<body>
+    <table style="border: 1px solid black; border-collapse: collapse;">
+        <?php
+            
+            $num_filas = 3;
+            $num_colum = 4;
+            for($i = 0; $i <= $num_filas; $i++){
+                echo "<tr>";
+                echo "<td style='border: 1px solid black; padding: 5px;'>".chr(65 + $i)."</td>";
+                
+                for($j = 1; $j <= $num_colum; $j++){
+                    echo "<td style='border: 1px solid black; padding: 5px;'>" . ($i + $j) . "</td>";
+                }
+                echo "</tr>";
+            }
+        ?>
+    </table>
+</body>
+</html>
