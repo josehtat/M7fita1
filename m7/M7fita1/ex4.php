0baf6e79bd4582be718b43f12dac34e94625da84
josehtat <150950561+josehtat@users.noreply.github.com>
2024-09-26 10:53:05 +0200

    ejercicio4 "finalizado"

--- m7/M7fita1/ex4.php
+++ m7/M7fita1/ex4.php
@@ -6,17 +6,24 @@
     <title>Ejercicio 4</title>
 </head>
 <body>
-    <table style="border: 1px solid black; border-collapse: collapse;">
+<table style="border: 1px solid black; border-collapse: collapse;">
         <?php
-            
             $num_filas = 3;
             $num_colum = 4;
-            for($i = 0; $i <= $num_filas; $i++){
+
+            for ($i = 0; $i <= $num_filas; $i++) {
                 echo "<tr>";
-                echo "<td style='border: 1px solid black; padding: 5px;'>".chr(65 + $i)."</td>";
-                
-                for($j = 1; $j <= $num_colum; $j++){
-                    echo "<td style='border: 1px solid black; padding: 5px;'>" . ($i + $j) . "</td>";
+
+                for ($j = 0; $j <= $num_colum; $j++) {
+                    if ($i == 0 && $j == 0) {
+                        echo "<td style='border: 1px solid black; padding: 5px;'>&nbsp;</td>";
+                    } elseif ($i == 0) {
+                        echo "<td style='border: 1px solid black; padding: 5px;'>".chr(65 + $j - 1)."</td>";
+                    } elseif ($j == 0) {
+                        echo "<td style='border: 1px solid black; padding: 5px;'>".$i."</td>";
+                    } else {
+                        echo "<td style='border: 1px solid black; padding: 5px;'>&nbsp;</td>";
+                    }
                 }
                 echo "</tr>";
             }
